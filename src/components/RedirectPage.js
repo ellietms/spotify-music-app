b2cea6177cc8e59b0addd8b89fce3d23c09544a5
ellie <ellie.tms1371@gmail.com>
2020-09-21 23:19:06 +0100

    add RedirectPage and Dashboard Components

---
+++ src/components/RedirectPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const RedirectPage = () => {
+    return(
+        <div className="">
+            Redirect Page
+        </div>
+    )
+}
+
+export default RedirectPage;
