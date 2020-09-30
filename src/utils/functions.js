690c13f92db3ab1ae437595b5a494cebf6a35101
ellie <ellie.tms1371@gmail.com>
2020-09-30 17:09:14 +0100

    add some comments in functionjs file and add some description

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// getParamValues function that will store the access_token, token_type and expires_in  
+// values in an object which will look like this:
 const getParamValues = (url) => {
     return(
         url.split(`#`)[1].split(`&`)
