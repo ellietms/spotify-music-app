db6508259af75b4d6721d54c7ce667b6e6203ec2
ellie <ellie.tms1371@gmail.com>
2020-09-30 21:28:46 +0100

    added a function for access the URL parameters and store them in local storage

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
-
-
+import _ from 'lodash';
+import {getParamValues} from '../utils/functions';
 
 const RedirectPage = () => {
+    function componentDidMount(){
+      const {setExpiryTime , history , location} = props; 
+      try{
+        if(_.isEmpty(location.hash)){
+            return history.push('/dashboard')
+        }
+        //result is a object with access_token and expiry_time and their values
+        const access_token = getParamValues(location);
+        //convert to milliseconds
+        const expiryTime = new Date().getTime() + access_token.expires_in*1000;
+        //check function.js component 
+        localStorage.setItem('params',JSON.stringify(access_token));
+        localStorage.setItem('expiry_time',expiryTime);
+        history.push('/dashboard');
+      } 
+      catch(error){
+        history.push('/');
+      }
+    }
     return(
-        <div className="">
-            Redirect Page
-        </div>
+        null
     );
 };
 
