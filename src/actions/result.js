78540d2c5871c2782bd166d2122e4e0d198d87a9
ellie <ellie.tms1371@gmail.com>
2020-10-05 19:53:55 +0100

    add descriptions

--- src/actions/result.js
+++ src/actions/result.js
@@ -1,2 +1,39 @@
-import {SET_ALBUMS,ADD_ALBUMS,SET_ARTISTS,ADD_ARTISTS,SET_PLAYLIST,ADD_PLAYLIST} from '../utils/constants';
- 
+import { get } from "../utils/api";
+export const setAlbums = (albums) => ({
+  type: "SET_ALBUMS",
+  albums,
+});
+
+export const addAlbums = (albums) => ({
+  type: "ADD_ALBUMS",
+  albums,
+});
+
+export const setArtists = (artists) => ({
+  type: "SET_ARTISTS",
+  artists,
+});
+
+export const addArtists = (artists) => ({
+  type: "ADD_ARTISTS",
+  artists,
+});
+
+export const setPlaylist = (playlists) => ({
+  type: "SET_PLAYLIST",
+  playlists,
+});
+
+export const addPlaylist = (playlists) => ({
+  type: "ADD_PLAYLIST",
+  playlists,
+});
+
+export const initiateGetResult = (searchTerm) => {
+  return async (dispatch) => {
+    try {
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+};
